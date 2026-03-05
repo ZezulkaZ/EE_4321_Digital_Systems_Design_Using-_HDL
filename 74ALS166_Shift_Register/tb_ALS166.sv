27946045e91a32922ed9d248b456b6e1dbeb0c4f
ZaneZezulka <88614824+ZezulkaZ@users.noreply.github.com>
2026-03-05 17:00:20 -0600

    Clean up tb_ALS166.sv by removing comments
    Removed header comments and unnecessary lines.

--- 74ALS166_Shift_Register/tb_ALS166.sv
+++ 74ALS166_Shift_Register/tb_ALS166.sv
@@ -1,24 +1,3 @@
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/16/2026 04:16:25 PM
-// Design Name: 
-// Module Name: tb_ALS166
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module tb_ALS166(clr, sh_ld, clk_inh, clk, ser, input_bus, q);
 
 output logic clr, sh_ld, clk_inh, clk, ser;
